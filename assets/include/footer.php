216f6d2ce309436d6cd0ee6dc465b37036c2c2c4
aniketnangare23 <aniketnangare23@gmail.com>
2022-08-08 18:32:22 +0530

    add footer logo

--- assets/include/footer.php
+++ assets/include/footer.php
@@ -2,9 +2,14 @@
 <footer class="ct-footer footer-3 footer-dark">
   <div class="container">
     <div class="row">
-      <div class="col-lg-7">
+      <div class="col-lg-8">
         <div class="row">
-          <div class="col-md-4 col-sm-6 footer-widget">
+          <div class="col-md-5 col-sm-6 footer-widget">
+            <img src="assets/img/logo/footer_logo.png" alt="logo" style="height:100px;">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, eligendi?</p>
+               <a href="https://wa.me/+919819164482" target="_blank"> <i class="fab fa-whatsapp" style="font-size:35px; color:red;"></i></a>
+          </div>
+          <div class="col-md-3 col-sm-6 footer-widget">
             <h5 class="widget-title">Spicess</h5>
             <ul>
               <li> <a href="product-details.php">Agro</a> </li>
@@ -13,7 +18,7 @@
               <li> <a href="product-details.php">Masalas Garam</a> </li>
             </ul>
           </div>
-          <div class="col-md-4 col-sm-6 footer-widget">
+          <div class="col-md-3 col-sm-6 footer-widget">
             <h5 class="widget-title">Legal</h5>
             <ul>
               <li> <a href="#">Privacy Policy</a> </li>
@@ -22,30 +27,20 @@
             </ul>
 
           </div>
-          <div class="col-md-4 col-sm-6 footer-widget">
-            <h5 class="widget-title">Payment & Shipping</h5>
-            <ul>
-              <li> <a href="#">Terms Of Use</a> </li>
-              <li> <a href="#">Payment Methods</a> </li>
-              <li> <a href="#">Shipping Guide </a> </li>
-              <li> <a href="#">Estimated Delievery Time</a> </li>
-            </ul>
-
-          </div>
         </div>
       </div>
-      <div class="col-lg-5 footer-widget contact-widget">
+      <div class="col-lg-4 footer-widget contact-widget">
         <h5 class="widget-title">Get In Touch</h5>
         <ul>
           <li>
             <a href="mailto:shreeaaipratishthan@gmail.com">
-              <i class="flaticon-email"></i>
+              <i class="flaticon-email" style="color:red"></i>
               shreeaaipratishthan@gmail.com
             </a>
           </li>
           <li>
             <a href="https://goo.gl/maps/dH9vJPJkTkmZZPke6" target="_blank">
-              <i class="flaticon-location"></i>
+              <i class="flaticon-location" style="color:red"></i>
               Shop No.4, Yashwant CHS., Sector 1, Plot 1, Palm Beach, Sanpada, Navi Mumbai-400 705.</a>
           </li>
         </ul>
@@ -56,7 +51,7 @@
     <div class="container">
       <div class="row align-items-center">
         <div class="col-lg-6 text-center text-lg-left mb-3 mb-lg-0">
-          <p class="m-0">&copy; 2022 <a href="#">Shree Aai Foods </a> | All Rights Reserved.</p>
+          <p class="m-0">&copy; 2022 <a href="masala.tectignis.in">Shree Aai Foods </a> | All Rights Reserved.</p>
         </div>
         <div class="col-lg-6 footer-widget pb-0 text-center text-lg-right">
           <div class="payment-image">
